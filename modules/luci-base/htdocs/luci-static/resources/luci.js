13a01fd02e258260561e2fe51deb0a93c98ab45f
Liangbin Lian <jjm2473@gmail.com>
2025-01-09 16:09:02 +0800

    luci-base: L.require supports custom version
    Signed-off-by: Liangbin Lian <jjm2473@gmail.com>

--- modules/luci-base/htdocs/luci-static/resources/luci.js
+++ modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -2385,7 +2385,7 @@
 		 * @returns {Promise<LuCI.baseclass>}
 		 * Returns the instantiated class.
 		 */
-		require(name, from = []) {
+		require(name, from = [], version) {
 			const L = this;
 			let url = null;
 
@@ -2400,7 +2400,9 @@
 				return Promise.resolve(classes[name]);
 			}
 
-			url = '%s/%s.js%s'.format(env.base_url, name.replace(/\./g, '/'), (env.resource_version ? `?v=${env.resource_version}` : ''));
+			version = version || env.resource_version;
+
+			url = '%s/%s.js%s'.format(env.base_url, name.replace(/\./g, '/'), (version ? '?v=' + version : ''));
 			from = [ name ].concat(from);
 
 			const compileClass = res => {
