51a16fc8be23ca32cf29d17ff5710e7708f0d0e6
Liangbin Lian <jjm2473@gmail.com>
2025-12-25 14:45:08 +0800

    luci.mk: postinst-pkg compat with openwrt 24.10+
    Signed-off-by: Liangbin Lian <jjm2473@gmail.com>

--- luci.mk
+++ luci.mk
@@ -214,7 +214,7 @@ ifndef Package/$(PKG_NAME)/postinst
 define Package/$(PKG_NAME)/postinst
 [ -n "$${IPKG_INSTROOT}" ] || {$(foreach script,$(LUCI_DEFAULTS),
 	[ -f /etc/uci-defaults/$(script) ] && (. /etc/uci-defaults/$(script)) && rm -f /etc/uci-defaults/$(script))
-	rm -f /tmp/luci-indexcache
+	rm -f /tmp/luci-indexcache /tmp/luci-indexcache.*
 	rm -rf /tmp/luci-modulecache/
 	killall -HUP rpcd 2>/dev/null
 	exit 0
